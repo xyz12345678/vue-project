f2eb8816374ce32e4e9e5dde14ca25768b01d13d
Xuefang Yang(杨雪芳) <xuefang.yang@msxf.com>
2017-10-25 18:52:12 +0800

    update

---
+++ src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app-4">
+    <ol>
+        <li v-for="(todo,index) in todos">
+        {{ todo.text}}-{{index}}
+        </li>
+    </ol>
+    <ul>
+        <li v-for="(value,key) in listobj">
+        {{ key }}: {{ value }}
+        </li>
+    </ul>
+    <div>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="text">文字按钮</el-button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+        value1: true,
+        value2: true,
+        todos: [
+            { text: '学习 JavaScript' },
+            { text: '学习 Vue' },
+            { text: '整个牛项目' }
+        ],
+        listobj:{
+            test1:"ewrwer",
+            test2:"w2werwer",
+            test3:"234234"
+        }
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+#app-4{
+    background-color: red;
+    ol{
+        color: #999;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Button, Select } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+// 设置语言
+locale.use(lang)
+
+// 引入组件
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 
 Vue.config.productionTip = false
 
@@ -11,5 +21,7 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App 
+    
+  }
 })
